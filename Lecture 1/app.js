2191cce1bc5bb1d42d3cb2a099cda8b27b481c79
R <658738@cognizant.com>
2018-07-10 12:19:33 +0530

    Modified app.js and calci app

--- Lecture 1/app.js
+++ Lecture 1/app.js
@@ -10,3 +10,27 @@
         };
     });
 })();
+
+
+(function(){
+    'use strict';
+    angular.module('nameCalculatorApp',[])
+    .controller('nameCalcController',function($scope){
+       $scope.name = "";
+       $scope.totalValue = 0;
+       $scope.displayNumeric = function(){
+           var totalStringValue = calculateTotal($scope.name);
+           $scope.totalValue = totalStringValue;
+       };
+
+       function calculateTotal(string)
+       {
+           var stringTotal = 0;
+           for(var i=0;i<string.length;i++)
+           {
+              stringTotal += string.charCodeAt(i);
+           }
+           return stringTotal;
+       }
+    });
+})();

a7a399ea45b54ea2f9e2210ee161ffd912f9da05
priyangaprakash <33112410+priyangaprakash@users.noreply.github.com>
2018-08-20 15:56:56 +0530

    Add files via upload
    recent

--- app1.js
+++ app1.js
@@ -11,18 +11,36 @@
  NarrowItDownController.$inject = ['MatchSearchService'];
 function NarrowItDownController(MatchSearchService) {
     var menu = this;
-    var items = [];
+    var items = this;
     var check = [];
-    menu.desc = " chicken ";
-    //var citems = [];
-    var promise = MatchSearchService.getMenuItems();
-    promise.then(function (response) {
-      menu.items = response.data.menu_items;
-      console.log(menu.items[0]);  
+    menu.desc = "";
+    menu.getSearchedMenuItems = function(desc)
+    {
+      var promise = MatchSearchService.getMenuItems(desc); 
+
+   //single value
+   /*
+    promise.then(function (success) 
+    {
+      menu.items = success;
+      console.log("menu:",menu.items);  
     })
     .catch(function (error) {
       console.log("Something went terribly wrong.");
     });
+    */
+
+    //array of values
+   promise.then(function (success) 
+   {
+     menu.check = success;
+     console.log("menu:",menu.check[0].name); //its an array  
+   })
+   .catch(function (error) {
+     console.log("Something went terribly wrong.");
+   });
+    }
+   
   
     
 }
@@ -32,16 +50,50 @@ function MatchSearchService($http, ApiBasePath)
 {
     var service = this;
     var items = [];
-    service.getMenuItems = function () {
+   // var desc = this;
+    service.getMenuItems = function (desc) {
     var response = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")       
     });
+    var d = desc;
+    console.log("descr:",d);  
+    return response.then(function(success)
+    {
+      //returning a single value
+      /*
+          desc = success.data.menu_items[0].description;
+          console.log("desc:",desc);
+          return desc;
+      */
+
+
+      //returning multiple value
+
+      items = success.data.menu_items;
+      console.log("Length:",items.length);
+      console.log("desc list:",items[0].description);
+     
+      if(items[0].description.search(desc)!=-1)
+      {
+        console.log("descr: inside",desc);
+        console.log("true");
+      }
+      else{
+        console.log("false");
+      }
+
+
+      var c = new RegExp(desc);
+      if(c.test(items[0].description)){
+         console.log("tr");
+      }
+      else{
+        console.log("fal");
+      }
+      return items;
+    }); 
    
-    service.items = response.$$state;
-    console.log("response",service.items.value);
-    return response;
-    
   };
 
 }
